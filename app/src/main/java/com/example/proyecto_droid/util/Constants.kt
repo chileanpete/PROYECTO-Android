88efa840e67d155ce05ef59759321d93fe0bd370
nparrac1 <npcruz630@gmail.com>
2025-07-06 18:56:15 -0400

    base funcional

--- app/src/main/java/com/example/proyecto_droid/util/Constants.kt
+++ app/src/main/java/com/example/proyecto_droid/util/Constants.kt
@@ -1,11 +1,11 @@
 package com.example.proyecto_droid.util
 
 object Constants {
-    const val BASE_URL = "http://192.168.1.10:8000"
+    const val BASE_URL = "http://192.168.0.6:8000"
 }
 
 
-Text(text = stringResource(id = R.string.titulo_objetivo))
-Text(text = stringResource(id = R.string.subtitulo_objetivo))
-Button(onClick = { ... }) {
-    Text(text = stringResource(id = R.string.aceptar))
+//Text(text = stringResource(id = R.string.titulo_objetivo))
+//Text(text = stringResource(id = R.string.subtitulo_objetivo))
+//(onClick = { ... }) {
+//Text(text = stringResource(id = R.string.aceptar))
